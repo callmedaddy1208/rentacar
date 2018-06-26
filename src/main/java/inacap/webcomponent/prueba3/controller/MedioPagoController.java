0f233c4f8383eb005016b0b3d73bd316a0e8f689
Administrador <Administrador@LAPTOP-JET1EO58>
2018-06-26 13:05:58 -0400

    envio de controller,model y repository de mediopago

---
+++ src/main/java/inacap/webcomponent/prueba3/controller/MedioPagoController.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba3.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import inacap.webcomponent.prueba3.model.MedioPagoModel;
+import inacap.webcomponent.prueba3.repository.MedioPagoRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+/**
+ *
+ * @author Administrador
+ */
+@RestController
+@RequestMapping("/mediopago")
+public class MedioPagoController {
+
+    @Autowired
+    private MedioPagoRepository medioPagoRepository;
+
+    @GetMapping()
+    public Iterable<MedioPagoModel> list() {
+
+        return medioPagoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MedioPagoModel> get(@PathVariable String id) {
+
+        Optional<MedioPagoModel> mpOptional = medioPagoRepository.findById(Integer.parseInt(id));
+
+        if (mpOptional.isPresent()) {
+            MedioPagoModel mpEncontrado = mpOptional.get();
+            return new ResponseEntity<>(mpEncontrado, HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MedioPagoModel> put(@PathVariable String id, @RequestBody MedioPagoModel medioPagoEditar) {
+
+        Optional<MedioPagoModel> mpOptional = medioPagoRepository.findById(Integer.parseInt(id));
+
+        if (mpOptional.isPresent()) {
+            MedioPagoModel mpEncontrado = mpOptional.get();
+
+            medioPagoEditar.setIdMedioPago(mpEncontrado.getIdMedioPago());
+            medioPagoRepository.save(medioPagoEditar);
+
+            return new ResponseEntity<>(medioPagoEditar, HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_MODIFIED);
+        }
+
+    }
+
+    @PostMapping
+    public ResponseEntity<MedioPagoModel> post(@RequestBody MedioPagoModel nuevoMedioPago) {
+       
+        nuevoMedioPago = medioPagoRepository.save(nuevoMedioPago);
+        
+        Optional<MedioPagoModel> mpOptional = medioPagoRepository.findById(nuevoMedioPago.getIdMedioPago());
+        
+        if (mpOptional.isPresent()){
+            MedioPagoModel mpEncontrado = mpOptional.get();
+            return new ResponseEntity<>(mpEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+        @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        
+        
+        Optional<MedioPagoModel> cOptional = medioPagoRepository.findById(Integer.parseInt(id));
+        
+        if (cOptional.isPresent()){
+            MedioPagoModel mpEncontrado = cOptional.get();
+            
+            medioPagoRepository.deleteById(mpEncontrado.getIdMedioPago());
+            
+            return new ResponseEntity<>(mpEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+     }
+
+}
---
+++ src/main/java/inacap/webcomponent/prueba3/model/MedioPagoModel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba3.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Administrador
+ */
+@Entity
+@Table(name = "medio_pago")
+public class MedioPagoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int idMedioPago;
+    private String nombreMedioPago;
+    private String detalle;
+
+    public int getIdMedioPago() {
+        return idMedioPago;
+    }
+
+    public void setIdMedioPago(int idMedioPago) {
+        this.idMedioPago = idMedioPago;
+    }
+
+    public String getNombreMedioPago() {
+        return nombreMedioPago;
+    }
+
+    public void setNombreMedioPago(String nombreMedioPago) {
+        this.nombreMedioPago = nombreMedioPago;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public MedioPagoModel() {
+    }
+
+    public MedioPagoModel(String nombreMedioPago, String detalle) {
+        this.nombreMedioPago = nombreMedioPago;
+        this.detalle = detalle;
+    }
+
+    private MedioPagoModel(int idMedioPago, String nombreMedioPago, String detalle) {
+        this.idMedioPago = idMedioPago;
+        this.nombreMedioPago = nombreMedioPago;
+        this.detalle = detalle;
+    }
+
+}
