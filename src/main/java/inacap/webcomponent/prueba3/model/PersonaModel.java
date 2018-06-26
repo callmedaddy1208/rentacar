f2c7b18a2b26c766444f12258c5c58a4c12922f3
Administrador <Administrador@LAPTOP-JET1EO58>
2018-06-26 11:16:02 -0400

    cree persona model

---
+++ src/main/java/inacap/webcomponent/prueba3/model/PersonaModel.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba3.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Administrador
+ */
+@Entity
+@Table(name = "persona")
+
+public class PersonaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int idPersona;
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private String telefono;
+    private String direccion1;
+    private String direccion2;
+    
+    @ManyToOne
+    @JoinColumn(name = "")
+    
+    private CiudadModel ciudad;
+    
+    
+    @ManyToOne
+    @JoinColumn(name = "")
+    
+    private TipoPersonaModel tipoPersona;
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion1(String direccion1) {
+        this.direccion1 = direccion1;
+    }
+
+    public void setDireccion2(String direccion2) {
+        this.direccion2 = direccion2;
+    }
+
+    public CiudadModel getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(CiudadModel ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public TipoPersonaModel getTipoPersona() {
+        return tipoPersona;
+    }
+
+    public void setTipoPersona(TipoPersonaModel tipoPersona) {
+        this.tipoPersona = tipoPersona;
+    }
+
+    public PersonaModel() {
+    }
+
+    public PersonaModel(String rut, String nombre, String apellido, Date fechaNacimiento, String telefono, String direccion1, String direccion2, CiudadModel ciudad, TipoPersonaModel tipoPersona) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.direccion1 = direccion1;
+        this.direccion2 = direccion2;
+        this.ciudad = ciudad;
+        this.tipoPersona = tipoPersona;
+    }
+
+    private PersonaModel(int idPersona, String rut, String nombre, String apellido, Date fechaNacimiento, String telefono, String direccion1, String direccion2, CiudadModel ciudad, TipoPersonaModel tipoPersona) {
+        this.idPersona = idPersona;
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.direccion1 = direccion1;
+        this.direccion2 = direccion2;
+        this.ciudad = ciudad;
+        this.tipoPersona = tipoPersona;
+    }
+
+   
+}
