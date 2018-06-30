b6840ea9514e5c323b314993c8721330c9b2d7b2
Administrador <Administrador@LAPTOP-JET1EO58>
2018-06-29 22:07:07 -0400

    envio de arriendo controller, model y repository

---
+++ src/main/java/inacap/webcomponent/prueba3/controller/ArriendoController.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba3.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import inacap.webcomponent.prueba3.model.ArriendoModel;
+import inacap.webcomponent.prueba3.repository.ArriendoRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+/**
+ *
+ * @author Administrador
+ */
+@RestController
+@RequestMapping("/url")
+public class ArriendoController {
+    
+    @Autowired
+    private ArriendoRepository arriendoRepository;
+    
+    @GetMapping()
+    public Iterable<ArriendoModel> list() {
+       
+         return arriendoRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<ArriendoModel> get(@PathVariable String id){
+        
+        Optional<ArriendoModel> aOptional = arriendoRepository.findById(Integer.parseInt(id));
+        
+        if (aOptional.isPresent()){
+            ArriendoModel aEncontrado = aOptional.get();
+            return new ResponseEntity<>(aEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<ArriendoModel> put(@PathVariable String id, @RequestBody ArriendoModel arriendoEditar) {
+        
+        Optional<ArriendoModel> cOptional = arriendoRepository.findById(Integer.parseInt(id));
+        
+        if (cOptional.isPresent()){
+            ArriendoModel cEncontrado = cOptional.get();
+            
+            arriendoEditar.setIdArriendo(cEncontrado.getIdArriendo());
+            arriendoRepository.save(arriendoEditar);
+            
+            return new ResponseEntity<>(arriendoEditar, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_MODIFIED);
+        }
+    
+   }  
+    
+    @PostMapping
+    public ResponseEntity<ArriendoModel> post(@RequestBody ArriendoModel nuevoArriendo) {
+       
+        nuevoArriendo = arriendoRepository.save(nuevoArriendo);
+        
+        Optional<ArriendoModel> aOptional = arriendoRepository.findById(nuevoArriendo.getIdArriendo());
+        
+        if (aOptional.isPresent()){
+            ArriendoModel aEncontrado = aOptional.get();
+            return new ResponseEntity<>(aEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        
+        
+        Optional<ArriendoModel> aOptional = arriendoRepository.findById(Integer.parseInt(id));
+        
+        if (aOptional.isPresent()){
+            ArriendoModel aEncontrado = aOptional.get();
+            
+            arriendoRepository.deleteById(aEncontrado.getIdArriendo());
+            
+            return new ResponseEntity<>(aEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+     } 
+}
---
+++ src/main/java/inacap/webcomponent/prueba3/model/ArriendoModel.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba3.model;
+
+import java.sql.Time;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Administrador
+ */
+@Entity
+ @Table(name="")
+public class ArriendoModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int idArriendo;
+    private Date fechaArriendo;
+    private Time horaArriendo;
+    
+    @ManyToOne
+    @JoinColumn(name = "")
+    private PersonaModel vendedor;
+    private PersonaModel cliente;
+    private VehiculoModel vehiculo;
+    private MedioPagoModel medioPago;
+
+    public int getIdArriendo() {
+        return idArriendo;
+    }
+
+    public void setIdArriendo(int idArriendo) {
+        this.idArriendo = idArriendo;
+    }
+
+    public Date getFechaArriendo() {
+        return fechaArriendo;
+    }
+
+    public void setFechaArriendo(Date fechaArriendo) {
+        this.fechaArriendo = fechaArriendo;
+    }
+
+    public Time getHoraArriendo() {
+        return horaArriendo;
+    }
+
+    public void setHoraArriendo(Time horaArriendo) {
+        this.horaArriendo = horaArriendo;
+    }
+
+    public PersonaModel getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(PersonaModel vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public PersonaModel getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(PersonaModel cliente) {
+        this.cliente = cliente;
+    }
+
+    public VehiculoModel getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(VehiculoModel vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public MedioPagoModel getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(MedioPagoModel medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public ArriendoModel() {
+    }
+
+    public ArriendoModel(Date fechaArriendo, Time horaArriendo, PersonaModel vendedor, PersonaModel cliente, VehiculoModel vehiculo, MedioPagoModel medioPago) {
+        this.fechaArriendo = fechaArriendo;
+        this.horaArriendo = horaArriendo;
+        this.vendedor = vendedor;
+        this.cliente = cliente;
+        this.vehiculo = vehiculo;
+        this.medioPago = medioPago;
+    }
+
+    public ArriendoModel(int idArriendo, Date fechaArriendo, Time horaArriendo, PersonaModel vendedor, PersonaModel cliente, VehiculoModel vehiculo, MedioPagoModel medioPago) {
+        this.idArriendo = idArriendo;
+        this.fechaArriendo = fechaArriendo;
+        this.horaArriendo = horaArriendo;
+        this.vendedor = vendedor;
+        this.cliente = cliente;
+        this.vehiculo = vehiculo;
+        this.medioPago = medioPago;
+    }
+    
+    
+    
+    
+}
