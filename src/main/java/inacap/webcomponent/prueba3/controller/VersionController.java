c2e83cbe3e6bda981f418b0536c23832a30b684b
pablo <pablo@10.24.9.29>
2018-07-03 09:55:05 -0400

    final

--- src/main/java/inacap/webcomponent/prueba3/controller/VersionController.java
+++ src/main/java/inacap/webcomponent/prueba3/controller/VersionController.java
@@ -7,7 +7,6 @@ package inacap.webcomponent.prueba3.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,38 +14,89 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import inacap.webcomponent.prueba3.model.VersionModel;
+import inacap.webcomponent.prueba3.repository.VersionRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 /**
  *
- * @author pablo
+ * @author Administrador
  */
 @RestController
-@RequestMapping("/url")
+@RequestMapping("/version")
 public class VersionController {
     
+    @Autowired
+    private VersionRepository versionRepository;
+    
     @GetMapping()
-    public List<Object> list() {
-        return null;
+    public Iterable<VersionModel> list() {
+       
+         return versionRepository.findAll();
     }
     
     @GetMapping("/{id}")
-    public Object get(@PathVariable String id) {
-        return null;
+    public ResponseEntity<VersionModel> get(@PathVariable String id){
+        
+        Optional<VersionModel> vOptional = versionRepository.findById(Integer.parseInt(id));
+        
+        if (vOptional.isPresent()){
+            VersionModel vEncontrado = vOptional.get();
+            return new ResponseEntity<>(vEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
-        return null;
-    }
+    public ResponseEntity<VersionModel> put(@PathVariable String id, @RequestBody VersionModel versionEditar) {
+        
+        Optional<VersionModel> vOptional = versionRepository.findById(Integer.parseInt(id));
+        
+        if (vOptional.isPresent()){
+            VersionModel vEncontrado = vOptional.get();
+            
+            versionEditar.setIdVersion(vEncontrado.getIdVersion());
+            versionRepository.save(versionEditar);
+            
+            return new ResponseEntity<>(versionEditar, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_MODIFIED);
+        }
+    
+   }  
     
     @PostMapping
-    public ResponseEntity<?> post(@RequestBody Object input) {
-        return null;
+    public ResponseEntity<VersionModel> post(@RequestBody VersionModel nuevaVersion) {
+       
+        nuevaVersion = versionRepository.save(nuevaVersion);
+        
+        Optional<VersionModel> vOptional = versionRepository.findById(nuevaVersion.getIdVersion());
+        
+        if (vOptional.isPresent()){
+            VersionModel vEncontrado = vOptional.get();
+            return new ResponseEntity<>(vEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable String id) {
-        return null;
-    }
-    
+        
+        
+        Optional<VersionModel> vOptional = versionRepository.findById(Integer.parseInt(id));
+        
+        if (vOptional.isPresent()){
+            VersionModel aEncontrado = vOptional.get();
+            
+            versionRepository.deleteById(aEncontrado.getIdVersion());
+            
+            return new ResponseEntity<>(aEncontrado, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+     } 
 }
